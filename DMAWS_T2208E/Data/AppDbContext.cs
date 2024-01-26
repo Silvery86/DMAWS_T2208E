b76537a67793507866a3f85b1a00e48e9153cb06
giang8692@gmail.com <giang8692@gmail.com>
2024-01-26 19:31:39 +0700

    Add project files.

---
+++ DMAWS_T2208E/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using DMAWS_T2208E.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+
+namespace DMAWS_T2208E.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Order> OrderTbl { get; set; }
+    }
+}
---
+++ DMAWS_T2208E/Models/Order.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DMAWS_T2208E.Models
+{
+    [Table("OrderTbl")]
+    public class Order
+    {
+        [Key]
+        public int ItemCode { get; set; }
+        public string ItemName { get; set; }
+        public int ItemQty { get; set; }
+        public string OrderDelivery { get; set; }
+        public string OrderAddress { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
